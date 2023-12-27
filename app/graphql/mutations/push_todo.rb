4c16537be8b6ffc153e08f14c91e8e51488ab9f0
Daniel <carreraprogrammer@gmail.com>
2023-12-27 15:23:40 -0500

    Add conflict detection and handling in PushTodo mutation

--- app/graphql/mutations/push_todo.rb
+++ app/graphql/mutations/push_todo.rb
@@ -2,20 +2,24 @@ module Mutations
   class PushTodo < BaseMutation
     argument :write_rows, [Types::TodoInputPushType], required: true
     field :todos, [Types::TodoType], null: false
+    field :conflicts, [Types::TodoType], null: false
 
     def resolve(write_rows:)
       updated_todos = []
+      conflicts = []
 
       write_rows.each do |row|
         # Extrae los estados asumido y nuevo del documento.
         assumed_master_state = row.assumedMasterState
         new_document_state = row.newDocumentState
 
-        todo = Todo.find_or_initialize_by(id: new_document_state.id)
+        todo = Todo.find_by(id: new_document_state.id)
 
-        # Si assumed_master_state es nil, significa que es una tarea nueva o una actualización sin gestión de conflictos.
-        if assumed_master_state.nil? || todo.new_record?
-          # Asigna los nuevos atributos al Todo.
+        # Verifica si hay conflictos.
+        if todo && conflict_detected?(todo, assumed_master_state)
+          conflicts.push(todo)
+        else
+          todo = Todo.new(id: new_document_state.id) unless todo
           update_todo_attributes(todo, new_document_state)
 
           if todo.save
@@ -23,22 +27,29 @@ module Mutations
           else
             puts "Error saving Todo ID: #{todo.id}, Errors: #{todo.errors.full_messages}"
           end
-        else
-          puts "Skipping update for Todo ID: #{todo.id} due to potential conflict"
         end
       end
 
-      { todos: updated_todos }
+      puts "Actualizados: #{updated_todos.map(&:id)}"
+      puts "Conflictos: #{conflicts.map(&:id)}"
+
+      { conflicts: conflicts, todos: updated_todos }
     end
 
     private
 
-    # Método para actualizar los atributos de Todo
+    def conflict_detected?(todo, assumed_state)
+      return false if assumed_state.nil?
+
+      todo.updatedAt != assumed_state.updatedAt
+    end
+
     def update_todo_attributes(todo, new_document_state)
       todo.assign_attributes(
         text: new_document_state.text,
         is_completed: new_document_state.isCompleted,
-        deleted: new_document_state.deleted
+        deleted: new_document_state.deleted,
+        updated_at: new_document_state.updatedAt
       )
     end
   end
