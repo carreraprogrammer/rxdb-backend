846a2ff629f2999de58cfcf7e1dce3b1df15f420
Daniel <carreraprogrammer@gmail.com>
2023-12-27 11:27:59 -0500

    Update seed data with current date and time

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 # db/seeds.rb
 Todo.destroy_all
 
+# Creando registros con la fecha y hora actual
 Todo.create([
-  { text: 'Comprar leche', is_completed: false, deleted: false, created_at: 2.days.ago, updated_at: 2.days.ago },
-  { text: 'Enviar correo al cliente', is_completed: true, deleted: false, created_at: 10.days.ago, updated_at: 1.day.ago },
-  { text: 'Llamar al doctor', is_completed: false, deleted: false, created_at: 5.days.ago, updated_at: 5.days.ago },
-  { text: 'Leer un libro', is_completed: true, deleted: false, created_at: 3.days.ago, updated_at: 3.days.ago },
-  { text: 'Practicar yoga', is_completed: false, deleted: true, created_at: 4.days.ago, updated_at: 4.days.ago },
-  { text: 'Revisar el proyecto', is_completed: true, deleted: false, created_at: 6.days.ago, updated_at: 2.hours.ago }
+  { text: 'Comprar leche', is_completed: false, deleted: false, created_at: Time.now, updated_at: Time.now },
+  { text: 'Enviar correo al cliente', is_completed: true, deleted: false, created_at: Time.now, updated_at: Time.now },
+  { text: 'Llamar al doctor', is_completed: false, deleted: false, created_at: Time.now, updated_at: Time.now },
+  { text: 'Leer un libro', is_completed: true, deleted: false, created_at: Time.now, updated_at: Time.now },
+  { text: 'Practicar yoga', is_completed: false, deleted: false, created_at: Time.now, updated_at: Time.now },
+  { text: 'Revisar el proyecto', is_completed: true, deleted: false, created_at: Time.now, updated_at: Time.now }
 ])
 
 puts "Datos de prueba creados."
