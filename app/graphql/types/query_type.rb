0a0b081ed61ae437e3d64f98008d7e40bb50a48c
Daniel <carreraprogrammer@gmail.com>
2023-12-28 08:13:59 -0500

    Refactor sync_todos method in QueryType.rb

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Types
   class QueryType < Types::BaseObject
     field :sync_todos, SyncTodosReturnType, null: false do
@@ -9,37 +7,32 @@ module Types
     
     field :todos, [TodoType], null: false
 
-    field :checkpoint, Types::CheckpointType, null: false
-
     def todos
       Todo.all
     end
 
     def sync_todos(checkpoint: nil, limit: nil)
-      if checkpoint
-        min_id = checkpoint[:id]
-        min_updated_at = checkpoint[:updated_at]
-        query = Todo.where('updated_at > ? OR (updated_at = ? AND id > ?)', min_updated_at, min_updated_at, min_id)
-      else
-        query = Todo.all
-      end
+      min_id = checkpoint ? checkpoint[:id] : nil
+      min_updated_at = checkpoint ? checkpoint[:updated_at] : nil
+
+      query = if checkpoint
+                Todo.where('updated_at > ? OR (updated_at = ? AND id > ?)', min_updated_at, min_updated_at, min_id)
+              else
+                Todo.all
+              end
 
       todos_query = limit ? query.order(updated_at: :asc, id: :asc).limit(limit) : query.order(updated_at: :asc, id: :asc)
+      last_todo = todos_query.last
+
+      new_checkpoint = if last_todo
+                         { id: last_todo.id, updated_at: last_todo.updated_at.iso8601 }
+                       else
+                         checkpoint ? { id: min_id, updated_at: min_updated_at } : nil
+                       end
 
       {
         documents: todos_query,
-        checkpoint: current_checkpoint
-      }
-    end
-
-    # Método para obtener el último checkpoint
-    def current_checkpoint
-      last_todo = Todo.order(updated_at: :desc, id: :desc).first
-      return {} unless last_todo
-
-      {
-        id: last_todo.id,
-        updated_at: last_todo.updated_at.iso8601
+        checkpoint: new_checkpoint
       }
     end
   end
